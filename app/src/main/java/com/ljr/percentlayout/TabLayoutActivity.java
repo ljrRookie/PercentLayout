f95e09bbbbf0aab86a688dcb3a13d7bda7b3ceed
ljtRookie <15622732935@163.com>
2017-03-27 19:47:16 +0800

    实现百分比相对，线性，帧布局

---
+++ app/src/main/java/com/ljr/percentlayout/TabLayoutActivity.java
@@ -0,0 +1,46 @@
+package com.ljr.percentlayout;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class TabLayoutActivity extends AppCompatActivity {
+
+    private TabLayout mMainTab;
+    private ViewPager mMainVp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tab_layout);
+        mMainTab = (TabLayout) findViewById(R.id.main_tab);
+        mMainVp = (ViewPager) findViewById(R.id.main_vp);
+initViewPager();
+    }
+
+    private void initViewPager() {
+        ArrayList<String> titles = new ArrayList<>();
+        titles.add("百分比线性布局");
+        titles.add("百分比相对布局");
+        titles.add("百分比帧布局");
+        for(int i=0;i<titles.size();i++){
+            mMainTab.addTab(mMainTab.newTab().setText(titles.get(i)));
+        }
+        ArrayList<Fragment> fragments = new ArrayList<Fragment>();
+        LinearFragment linearFragment = new LinearFragment();
+        RelativeFragment relativeFragment = new RelativeFragment();
+        FrameFragment frameFragment = new FrameFragment();
+        fragments.add(linearFragment);
+        fragments.add(relativeFragment);
+        fragments.add(frameFragment);
+
+        FragmentAdapter fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), fragments, titles);
+        mMainVp.setAdapter(fragmentAdapter);
+        mMainTab.setupWithViewPager(mMainVp);
+        mMainTab.setTabsFromPagerAdapter(fragmentAdapter);
+    }
+}
